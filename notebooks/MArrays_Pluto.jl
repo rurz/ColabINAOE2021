80435ad7cf71b8347463ecfaa7ab68dd933299eb
arurz <arurz@inaoep.mx>
2021-03-08 23:20:12 -0600

    Previous results reproduced
    * Completed scripts previously developed
    * Reproduced the results founded in the PhD_Calcs repo

--- notebooks/MArrays_Pluto.jl
+++ notebooks/MArrays_Pluto.jl
@@ -32,28 +32,21 @@ end;
 # ╔═╡ 81e6e088-7bd9-11eb-17a6-03ef56dc026b
 # Initial amplitude vector constructors
 
-pint(q) = sparsevec(Dict(q + 1 => 1), N); # Single amplitude point at 0 ≤ q ≤ N
+pint(q) = sparsevec(Dict(q + 1 => 1), N + 1); # Single amplitude point at 0 ≤ q ≤ N
 
 # ╔═╡ e3ceb6d0-7bda-11eb-115f-4584602f29df
 # Initial amplitude vector
 
-q0 = normalize(pint(16));
+q0 = normalize(pint(8) + pint(24));
 
 # ╔═╡ 203134d6-7bdb-11eb-2df1-374c190cc45b
-begin 
-	t_list = range(0,stop=60,length=200)
-	lt = length(t_list)
-end;
+t_list = range(0, stop = 61, length = 200);
 
-# ╔═╡ 77034402-7bdb-11eb-3b30-3dc93d022393
-begin
-	evolc = zeros(Float64,(N,lt))
-	for n in 1:N
-		for t in 1:lt
-			evolc[n, t] = ColabINAOE2021.qc(k, n, t_list[t], N, Array(q0))
-		end
-	end
-end
+# ╔═╡ b2181d84-7fcf-11eb-2400-23cdf5b8fff7
+md"### 1. Circular array"
+
+# ╔═╡ b49a93f8-806b-11eb-2dc1-3f8e020d0f8c
+evolc = ColabINAOE2021.Qc(1, t_list, Array(q0));
 
 # ╔═╡ e30513a6-7bdb-11eb-0e31-cd6f16283698
 begin
@@ -63,18 +56,66 @@ begin
 	ax_1.set_xlabel("Evolution time: t")
 	ax_1.set_ylabel("Element index: n")
 	ax_1.set_aspect("2")
-	ax_1.set_xticks([0,50,100,150,200])
-	ax_1.set_xticklabels([0,15,30,45,60])
-	
-	pcm = ax_1.get_children()[10]
-	cb = colorbar(pcm,orientation="vertical",shrink=0.7,aspect=35,fraction=0.015)
-	
+	ax_1.set_xticks([0,33,66,99,132,165,200])
+	ax_1.set_xticklabels([0,10,20,30,40,50,60])
+
+	pcm1 = ax_1.get_children()[10]
+	cb1 = colorbar(pcm1,orientation="vertical",shrink=0.7,aspect=35,fraction=0.015)
+
 	tight_layout()
 	#savefig("notebooks/circ.png",dpi=150,transparent=true)
 end
 
 # ╔═╡ 07fccfdc-7bdc-11eb-1a00-a345a06f8517
 sum(abs2.(evolc))
+
+# ╔═╡ ac233d02-7fcf-11eb-1640-c172d67a0cfc
+md"### 2. Lineal array"
+
+# ╔═╡ cbc567c2-8095-11eb-0996-05753de896af
+evoll = ColabINAOE2021.Ql(1, t_list, Array(q0));
+
+# ╔═╡ fb886f34-8095-11eb-0b4f-e58f8dcb8964
+begin
+	fig_2 = figure(figsize = (10,5))
+	ax_2 = gca()
+	ax_2.imshow(evoll)
+	ax_2.set_xlabel("Evolution time: t")
+	ax_2.set_ylabel("Element index: n")
+	ax_2.set_aspect("2")
+	ax_2.set_xticks([0,33,66,99,132,165,200])
+	ax_2.set_xticklabels([0,10,20,30,40,50,60])
+
+	pcm2 = ax_2.get_children()[10]
+	cb2 = colorbar(pcm2,orientation="vertical",shrink=0.7,aspect=35,fraction=0.015)
+
+	tight_layout()
+	#savefig("notebooks/lin.png",dpi=150,transparent=true)
+end
+
+# ╔═╡ 64040048-8096-11eb-2c6f-ed400b9dbf42
+md"### 3. Kravchuk array "
+
+# ╔═╡ 6ec1d92e-8096-11eb-3980-7107878eecb7
+evolk = ColabINAOE2021.Qk(t_list, Array(q0));
+
+# ╔═╡ 7f1a6296-8096-11eb-12b1-a14e3a7eb885
+begin
+	fig_3 = figure(figsize = (10,5))
+	ax_3 = gca()
+	ax_3.imshow(evolk)
+	ax_3.set_xlabel("Evolution time: t")
+	ax_3.set_ylabel("Element index: n")
+	ax_3.set_aspect("2")
+	ax_3.set_xticks([0,33,66,99,132,165,200])
+	ax_3.set_xticklabels([0,10,20,30,40,50,60])
+
+	pcm3 = ax_2.get_children()[10]
+	cb3 = colorbar(pcm3,orientation="vertical",shrink=0.7,aspect=35,fraction=0.015)
+
+	tight_layout()
+	#savefig("notebooks/krav.png",dpi=150,transparent=true)
+end
 
 # ╔═╡ Cell order:
 # ╠═b6b1b198-7bd8-11eb-10f5-a98dd0a59fcf
@@ -84,6 +125,13 @@ sum(abs2.(evolc))
 # ╠═81e6e088-7bd9-11eb-17a6-03ef56dc026b
 # ╠═e3ceb6d0-7bda-11eb-115f-4584602f29df
 # ╠═203134d6-7bdb-11eb-2df1-374c190cc45b
-# ╠═77034402-7bdb-11eb-3b30-3dc93d022393
+# ╟─b2181d84-7fcf-11eb-2400-23cdf5b8fff7
+# ╠═b49a93f8-806b-11eb-2dc1-3f8e020d0f8c
 # ╠═e30513a6-7bdb-11eb-0e31-cd6f16283698
 # ╠═07fccfdc-7bdc-11eb-1a00-a345a06f8517
+# ╟─ac233d02-7fcf-11eb-1640-c172d67a0cfc
+# ╠═cbc567c2-8095-11eb-0996-05753de896af
+# ╠═fb886f34-8095-11eb-0b4f-e58f8dcb8964
+# ╟─64040048-8096-11eb-2c6f-ed400b9dbf42
+# ╠═6ec1d92e-8096-11eb-3980-7107878eecb7
+# ╠═7f1a6296-8096-11eb-12b1-a14e3a7eb885
--- src/MArrays.jl
+++ src/MArrays.jl
@@ -1,24 +1,179 @@
+using SpecialPolynomials
+using HypergeometricFunctions
+
 """ MArrays.jl contains the functions and methods for the evolution of initial
 amplitudes in a one-dimensional masses array with harmonic coupling. Here, we
 study different coupling laws, their evolution on time, and their energy content """
 
-""" Circular arrangement with equal strength coupling """
+# 1. ---------------------------------------------------------------------------
 
-"Amplitude and argument functions"
+"""### Circular arrangement with equal strength coupling: k_{j}\equiv j \forall j """
+
+"* Amplitude and argument functions"
 Ac(N) = 1/(N + 1)
 ωc(m, N) = (π * m) * Ac(N)
 
-"Kernel function"
+"* Kernel function"
 kec(t, k, m, n, l, N) = cos(2 * t * (√k) * sin(ωc(m, N))) * cos(2 * ωc(m, N) * (n - l))
 
-"qc is the main function who returns the amplitude in the _n_ position at the
+"* qc is the main function who returns the amplitude in the _n_ position at the
 _t_ time"
 function qc(k, n, t, N, V::Vector)
-    u = zeros(Float64,(N,N))
-    for m in 0:(N-1)
-        for l in 0:(N-1)
-            u[l + 1, m + 1] = kec(t, k, m + 1, n, l + 1, N) * V[l + 1]
+    u = zeros(Float64,(N + 1, N + 1))
+    for m in 0:N
+        for l in 0:N
+            u[l + 1, m + 1] = kec(t, k, m, n, l, N) * V[l + 1]
         end
     end
     return Ac(N) * sum(u)
+end
+
+"""* Qc is an ad-hoc function to evaluate the matrix evolution consisting on
+a M-dimensional time array T and a N-dimensional initial amplitudes vector V_{0}.
+So, we arrive to a matrix of N \times M. The function requires the value of the
+restitution constant k, the time array T and the initial amplitudes vector V_{0} as
+Qc(k, T:Array, V_{0}:Vector) """
+
+function Qc(k, t, V::Vector)
+    N = length(V)-1
+    T = length(t)
+    v = zeros(Float64,(N + 1,T))
+    for l in 0:N
+        for m in 1:T
+            v[l + 1, m] = qc(k, l, t[m], N, V)
+        end
+    end
+    return v
+end
+
+# The initial amplitudes vector V need to be of dimension N + 1 = dim,
+# because the loops starts counting at 0.
+
+# 2. ---------------------------------------------------------------------------
+
+"""### Lineal array with specific conditions in the coupling matrix """
+
+"""##### m_{j} = 1, k_{j} = k, \forall j """
+
+""" We will use the generator method for obtain the second kind Chebyshev polynomial
+given by the package SpecialPolynomials.jl """
+
+"""* Generator array for the Chebyshev's """
+function gen(n)
+    gen = zeros(Int64, n + 1)
+    gen[end] = 1
+    return gen
+end
+
+"""* Chebyshev polynomial U_{n}(x) """
+function chebU(x, n)
+    return SpecialPolynomials.ChebyshevU(gen(n))(x)
+end
+
+""" Phase and zeros of the Chebyshev's """
+ϕ(j, N) = (π * j) / (N + 2)
+y(j, N) = cos(ϕ(j, N))
+
+""" Oscillatory term """
+oscl(k, t, j, N) = cos(2 * t * (√k) * sin(ϕ(j + 1, N)/2))
+
+""" Position dependent normalization """
+function nc(j, N)
+    nc = zeros(N + 1)
+    for l in 0:N
+        nc[l + 1] = (chebU(y(j + 1, N), l))^2
+    end
+    return sum(nc)
+end
+
+""" Two-dimensional Chebyshev polymonial """
+twocheb(j, l, n, N) = chebU(y(j + 1, N), l) * chebU(y(j + 1, N), n)
+
+""" Kernel composition function """
+kel(t, k, n, l, j, N) = (twocheb(j, l, n, N)/nc(j, N)) * oscl(k, t, j, N)
+
+""" q_{l} is the main function for the lineal array evolution, when evaluated
+    for some vector V of size N, it gives a N \times N matrix who is a function
+    time t and position index n """
+function ql(k, n, t, N, V::Vector)
+    nr = [nc(j, N) for j in 0:N]
+    tc(j, l, n) = twocheb(j, l, n, N)
+    osc(k, t, j) = oscl(k, t, j, N)
+    el = zeros(Float64,(N + 1, N + 1))
+    for l in 0:N
+        for j in 0:N
+            el[l + 1, j + 1] = (tc(j, l, n) / nr[j + 1]) * osc(k, t, j) * V[l + 1]
+        end
+    end
+    return sum(el)
+end
+
+""" Q_{l} is, again, an ad-hoc function for evalute the amplitude evolution
+    function q_{l}(n, t) """
+function Ql(k, t, V::Vector)
+    dn = length(V)-1
+    ell = zeros(Float64, (dn + 1, length(t)))
+    for n in 0:dn
+        for T in 1:length(t)
+            ell[n + 1, T] = ql(k, n, t[T], dn, V)
+        end
+    end
+    return ell
+end
+
+# 3. Kravchuk ------------------------------------------------------------------
+
+""" Symmetric Kravchuk polynomial """
+function kp(i, j, N)
+    if i == j == N
+        return HypergeometricFunctions._₂F₁general2(0, -i, -N, 2)
+    else
+        return HypergeometricFunctions._₂F₁general2(-j, -i, -N, 2)
+    end
+end
+
+ω(j, N) = 2.0^(-N) * binomial(N, j)
+h(i, N) = 1 / binomial(N, i)
+
+oscc(t, n) = cos(t * sqrt(n))
+
+""" Symmetric Kravchuk function """
+kf(i, j, N) = sqrt(ω(j, N) / h(i, N)) * kp(i, j, N)
+
+""" Kravchuk matrix """
+function kfm(N)
+    km = zeros(Float64, (N + 1, N + 1))
+    for i in 0:N
+        for j in 0:N
+            km[i + 1, j + 1] = kf(i, j, N)
+        end
+    end
+    return km
+end
+
+""" Two-dimensional Kravchuk function """
+k2(m, l, n, N) = kf(m, n, N) * kf(l, n, N)
+
+""" q_{k} function """
+function qk(m, t, N, V::Vector)
+    u = zeros(Float64, (N + 1, N + 1))
+    for l in  0:N
+        for n in 0:N
+            u[l + 1, n + 1] = k2(m, l, n, N) * oscc(t, n) * V[l + 1]
+        end
+    end
+    return sum(u)
+end
+
+""" Q_{k} is the ad-hoc function for straight-forward evaluation """
+function Qk(t, V::Vector)
+    dn = length(V)-1
+    T = length(t)
+    ek = zeros(Float64, (dn + 1, T))
+    for m in 0:dn
+        for τ in 1:T
+            ek[m + 1, τ] = qk(m, t[τ], dn, V)
+        end
+    end
+    return ek
 end
